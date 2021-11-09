fe4ef98c311a948094b0e49bf18f1a2a368d9936
Sudar1977 <Sudar1977@gmail.com>
2021-11-09 15:37:45 +0300

    BreakfastWithUnity: 06TimeSlow

---
+++ Assets/06TimeSlow/ChangePitchToTimeScale.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangePitchToTimeScale : MonoBehaviour {
+	// Update is called once per frame
+	void Update ()
+	{
+		GetComponent<AudioSource>().pitch = Time.timeScale;
+	}
+}
