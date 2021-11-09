c72f5a04d1cec11e967d66bea07eb0a706bedee2
Sudar1977 <Sudar1977@gmail.com>
2021-11-09 14:56:24 +0300

    BreakfastWithUnity: 01Snake 02CameraShake + removedeadcomponentsfromscenes https://www.youtube.com/playlist?list=PLlHjNcdoyw6WVpwY7_InRc6vYFS3bEPar

---
+++ Assets/02CameraShake/CameraShakeOnStart.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShakeOnStart : MonoBehaviour
+{
+	public float amplitude = 0.1f;
+	public float duration = 0.5f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		CameraShake.Instance.Shake(amplitude,duration);
+	}
+}
