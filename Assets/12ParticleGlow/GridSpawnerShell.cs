aa5dd2aa8c6bfc495668073e591d7598bc2f6707
Sudar1977 <Sudar1977@gmail.com>
2021-11-10 18:38:57 +0300

    BreakfastWithUnity: 03LaserDot 09GridSpawner 10SelfReconstructing 11FunWithBuiltInShaders 12ParticleGlow 13LightningBall NEED FPSgit add .! 14ZeroG need CONTROLgit add .! 53ClickToMove need textyre and work

---
+++ Assets/12ParticleGlow/GridSpawnerShell.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Spawn/Grid Spawner Shell")]
+public class GridSpawnerShell : MonoBehaviour
+{
+	[Header("Required GameObject variables")]
+	[Tooltip("Object to spawn outside of grid")]
+	public GameObject shellObject;
+	[Tooltip("Object to spawn inside of grid")]
+	public GameObject interiorObject;
+
+	[Header("Tweak values")]
+	[Tooltip("Number of objects in the X direction")]
+	public int numObjectsX = 1;
+	[Tooltip("Number of objects in the Y direction")]
+	public int numObjectsY = 1;
+	[Tooltip("Number of objects in the Z direction")]
+	public int numObjectsZ = 1;
+
+	[Space(10.0f)]
+	public Vector3 objectSpacing = Vector3.one;
+
+	// Use this for initialization
+	void Start ()
+	{
+		for(int x = 0; x < numObjectsX; x++)
+		{
+			for(int y = 0; y < numObjectsY; y++)
+			{
+				for(int z = 0; z < numObjectsZ; z++)
+				{
+					if(x == 0 || y == 0 || z == 0 || x == numObjectsX-1 || y == numObjectsY-1 || z == numObjectsZ-1)
+					{
+						Instantiate(shellObject,transform.position+transform.right*x*objectSpacing.x+transform.up*y*objectSpacing.y+transform.forward*z*objectSpacing.z,Quaternion.identity);
+					}
+					else
+					{
+						Instantiate(interiorObject,transform.position+transform.right*x*objectSpacing.x+transform.up*y*objectSpacing.y+transform.forward*z*objectSpacing.z,Quaternion.identity);
+					}
+				}
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
---
+++ Assets/13LightningBall/RandomTextureOffset.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[AddComponentMenu("TextureEffects/Random Texture Offset")]
+public class RandomTextureOffset : MonoBehaviour 
+{
+	[System.Serializable]
+	public class MaterialInfo
+	{
+		public Material material;
+		public string textureName = "_MainTex";
+	}
+	
+	public List<MaterialInfo> materialInfoList = new List<MaterialInfo>();
+
+	// Update is called once per frame
+	void Update ()
+	{
+		foreach(MaterialInfo info in materialInfoList)
+		{
+			info.material.SetTextureOffset(info.textureName,new Vector2(Random.value,Random.value));
+		}
+	}
+
+	void OnDestroy()
+	{
+		foreach(MaterialInfo info in materialInfoList)
+		{
+			info.material.SetTextureOffset(info.textureName,Vector2.zero);
+		}
+	}
+}
