1219ce91d806b171fc39c100e38411160c5e304a
Sudar1977 <Sudar1977@gmail.com>
2021-11-12 10:03:54 +0300

    In 03LaserDot add MyDualTouchControls.prefab  'MyFirst Person Controller.prefab' Add MyPrefabs

--- Assets/03LaserDot/Mouse_Look.cs
+++ Assets/03LaserDot/Mouse_Look.cs
@@ -1,21 +1,27 @@
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
+//https://www.youtube.com/watch?v=wbn7kFVrM4g
 public class Mouse_Look : MonoBehaviour
 {
     public float Smoothness = 4; 
     public  Vector2 Sensitivity = new Vector2(4, 4); 
     private Vector2 NewCoord;
     [HideInInspector]
-    public  Vector2 CiurentCoord;
+    public  Vector2 CurrentCoord;
     public  Vector2 Limit = new Vector2(-70, 80);
     private Vector2 vel;
+
+    private void Awake()
+    {
+        gameObject.tag = "MainCamera";
+    }
     void Update()
     { 
         NewCoord.x = Mathf.Clamp(NewCoord.x, Limit.x,Limit.y);
-        NewCoord.x -= CrossPlatformInputManager.GetAxis("Vertical") *Sensitivity.x;
-        NewCoord.y += CrossPlatformInputManager.GetAxis("Horizontal") *Sensitivity.y;
-        CiurentCoord.x = Mathf.SmoothDamp(CiurentCoord.x, NewCoord.x, ref vel.x, Smoothness/100);
-        CiurentCoord.y = Mathf.SmoothDamp(CiurentCoord.y, NewCoord.y, ref vel.y, Smoothness/100); 
-        transform.rotation=Quaternion.Euler(CiurentCoord.x, CiurentCoord.y, 0);
+        NewCoord.x -= CrossPlatformInputManager.GetAxis("Mouse Y")*Sensitivity.x;
+        NewCoord.y += CrossPlatformInputManager.GetAxis("Mouse X")*Sensitivity.y;
+        CurrentCoord.x = Mathf.SmoothDamp(CurrentCoord.x, NewCoord.x, ref vel.x, Smoothness/100);
+        CurrentCoord.y = Mathf.SmoothDamp(CurrentCoord.y, NewCoord.y, ref vel.y, Smoothness/100); 
+        transform.rotation=Quaternion.Euler(CurrentCoord.x, CurrentCoord.y, 0);
     }
 }
---
+++ Assets/03LaserDot/MoveController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+//https://www.youtube.com/watch?v=wbn7kFVrM4g&list=PLv75qW_zhOpxGC6A5g-XAmb0NLonbSGYm&index=5
+[RequireComponent(typeof(Rigidbody))]
+public class MoveController : MonoBehaviour
+{
+    public Vector3 Speed;
+    private Mouse_Look Cam;
+    private Rigidbody Body;
+    private void Start()
+    {
+        Cam = GameObject.FindWithTag("MainCamera").GetComponent<Mouse_Look>();
+        Body = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(0, Cam.CurrentCoord.y, 0);
+        transform.Translate(Vector3.forward * CrossPlatformInputManager.GetAxis("Vertical") * Speed.x * Time.deltaTime);
+        transform.Translate(Vector3.right * CrossPlatformInputManager.GetAxis("Horizontal") * Speed.y * Time.deltaTime);
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Body.AddRelativeForce(0, Speed.z * 100, 0);
+            Debug.Log("Jump pressed");
+        }
+    }
+}
+
